eb376e94dfa9e866fb5f315e841978f9bbb14336
gheorghe835 <gheorghe835@gmail.com>
2025-02-04 08:47:12 +0200

    Functie ce inverseaza un string, Convertim stringul intr-un array de caractere

--- src/stringReverser/StringReverser.java
+++ src/stringReverser/StringReverser.java
@@ -1,8 +1,10 @@
 package stringReverser;
 
 public class StringReverser {
+    //Functie ce inverseaza un string
     public static String stringReverser(String text){
-        
+        //Convertim stringul intr-un array de caractere
+
         return ;
     }
 }

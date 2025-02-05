c958aee07e2a264875dfcf272cb63838bdece6a9
gheorghe835 <gheorghe835@gmail.com>
2025-02-05 19:10:25 +0200

    Adaugarea tascurilor

---
+++ src/tascuri/Print.java
@@ -0,0 +1,12 @@
+package tascuri;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("  Java Core.\n" +
+                "1.\tInversarea unui string: Scrie o funcție care inversează un string.\n" +
+                "2.\tVerificarea palindromului: Scrie o funcție care verifică dacă un string este palindrom.\n" +
+                "3.\tNumere prime: Scrie o funcție care verifică dacă un număr este prim.\n" +
+                "4.\tFibonacci: Scrie o funcție care generează primele N numere Fibonacci.\n" +
+                "5.\tFactorial: Scrie o funcție care calculează factorialul unui număr.\n");
+    }
+}

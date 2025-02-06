e619aad89f9691f92cb75cb891cc6a0579d421a3
gheorghe835 <gheorghe835@gmail.com>
2025-02-06 18:48:56 +0200

    adaugarea tascurilor 6 - 10

--- src/tascuri/Print.java
+++ src/tascuri/Print.java
@@ -7,6 +7,11 @@ public class Print {
                 "2.\tVerificarea palindromului: Scrie o funcție care verifică dacă un string este palindrom.\n" +
                 "3.\tNumere prime: Scrie o funcție care verifică dacă un număr este prim.\n" +
                 "4.\tFibonacci: Scrie o funcție care generează primele N numere Fibonacci.\n" +
-                "5.\tFactorial: Scrie o funcție care calculează factorialul unui număr.\n");
+                "5.\tFactorial: Scrie o funcție care calculează factorialul unui număr.\n"+
+                "6.\tSuma numerelor: Scrie o funcție care returnează suma numerelor dintr-un array.\n" +
+                "7.\tMaximul și minimul: Scrie o funcție care găsește valoarea maximă și minimă dintr-un array.\n" +
+                "8.\tSortarea unui array: Implementează algoritmul de sortare (ex: Bubble Sort, Quick Sort).\n" +
+                "9.\tCăutarea binară: Implementează algoritmul de căutare binară.\n" +
+                "10.\tNumerele pare: Scrie o funcție care returnează toate numerele pare dintr-un array.\n");
     }
 }

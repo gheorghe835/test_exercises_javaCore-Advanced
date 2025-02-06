62317ebf0e9b6aba1ed5b0db22d2b3e0a5ce0c9d
gheorghe835 <gheorghe835@gmail.com>
2025-02-06 19:45:22 +0200

    adaugarea conditiei tascului

--- src/sumArray/SumArray.java
+++ src/sumArray/SumArray.java
@@ -1,5 +1,7 @@
 package sumArray;
 
+//6.	Suma numerelor: Scrie o funcție care returnează suma numerelor dintr-un array.
+
 public class SumArray {
     public static int calculateSum(int[] numbers){
         int sum = 0;//initializam suma cu 0

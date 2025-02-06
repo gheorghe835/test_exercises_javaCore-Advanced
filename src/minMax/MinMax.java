72ca53019171bbe8f628e0f00d65a1f1cf3993bd
gheorghe835 <gheorghe835@gmail.com>
2025-02-06 20:08:33 +0200

    adaugarea logicii de cautare a mimimului si a maximului plus testarea functiei

--- src/minMax/MinMax.java
+++ src/minMax/MinMax.java
@@ -2,4 +2,25 @@ package minMax;
 
 //7.	Maximul și minimul: Scrie o funcție care găsește valoarea maximă și minimă dintr-un array
 public class MinMax {
+    public static void minMax(int[] numbers){
+        if(numbers.length == 0){
+            System.out.println("  Arrayul este gol.");
+            return;
+        }
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int number:
+             numbers) {
+            if(number < 0){
+                min = number;
+            }
+            if(number > max){
+                max = number;
+            }
+        }
+
+        System.out.println("  Valoarea minima:: " + min);
+        System.out.println("  Valoarea maxima:: " + max);
+    }
 }
---
+++ src/minMax/Print.java
@@ -0,0 +1,20 @@
+package minMax;
+
+import java.util.Scanner;
+
+import static minMax.MinMax.minMax;
+public class Print {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("  Introduceti numarul de elemente::");
+        int num = scanner.nextInt();
+
+        int[] numbers = new int[num];
+        System.out.println("  Introduceti " + num + " numere:: ");
+        for (int i = 0; i < num; i++){
+            numbers[i] = scanner.nextInt();
+        }
+        minMax(numbers);
+        scanner.close();
+    }
+}

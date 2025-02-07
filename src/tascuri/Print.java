094ec6174768f09519c0780d1875aa02548a8693
gheorghe835 <gheorghe835@gmail.com>
2025-02-07 07:38:39 +0200

    adaugarea tascurilor 11 - 15

--- src/tascuri/Print.java
+++ src/tascuri/Print.java
@@ -12,6 +12,11 @@ public class Print {
                 "7.\tMaximul și minimul: Scrie o funcție care găsește valoarea maximă și minimă dintr-un array.\n" +
                 "8.\tSortarea unui array: Implementează algoritmul de sortare (ex: Bubble Sort, Quick Sort).\n" +
                 "9.\tCăutarea binară: Implementează algoritmul de căutare binară.\n" +
-                "10.\tNumerele pare: Scrie o funcție care returnează toate numerele pare dintr-un array.\n");
+                "10.\tNumerele pare: Scrie o funcție care returnează toate numerele pare dintr-un array.\n"+
+                "11.\tNumerele impare: Scrie o funcție care returnează toate numerele impare dintr-un array.\n" +
+                "12.\tConcatenează două stringuri: Scrie o funcție care concatenează două stringuri.\n" +
+                "13.\tNumărul de apariții: Scrie o funcție care numără aparițiile unui caracter într-un string.\n" +
+                "14.\tAnagramă: Scrie o funcție care verifică dacă două stringuri sunt anagrame.\n" +
+                "15.\tRotirea unui array: Scrie o funcție care rotește un array cu K poziții.\n");
     }
 }
